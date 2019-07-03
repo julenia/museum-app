3078a80c21cd01e0366f0d22b7a59a5448f3b854
julenia <julia.jankowska@onet.eu>
2019-07-03 16:11:22 +0200

    Display elements and reset form

--- pages/detail-page.js
+++ pages/detail-page.js
@@ -6,12 +6,13 @@ function submitComment() {
     const comment = document.createElement('section');
     const h3 = document.createElement('h3');
     const p = document.createElement('p');
-    h3.innerHTML = '${name} said:';
+    h3.innerHTML = `${name} said:`;
     p.innerHTML = msg;
     comment.classList.add('comment');
     comment.appendChild(h3);
     comment.appendChild(p);
-
-    console.log(comment);
-
+    const commentSection = document.getElementById('comments');
+    commentSection.appendChild(comment);
+    inputField.value = null;
+    textArea.value = null;
 }
