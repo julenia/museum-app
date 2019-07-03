dfe9ddb66739b946feaf3691eca2070c0ca88c17
julenia <julia.jankowska@onet.eu>
2019-07-03 17:25:37 +0200

    Set Up Form Validation

--- pages/detail-page.js
+++ pages/detail-page.js
@@ -1,8 +1,36 @@
+function doesNotPassAllValidations (name, msg){
+    if(!name){
+        alert('You forgot to fill in your name!');
+        return true;
+    }
+    if(!msg){
+        alert('You forgot to fill in your message!');
+        return true;    
+    }
+    if(msg.length > 280){
+        alert('Your comment is too long!');
+        return true;
+    }
+    if(msg.toLowerCase().includes('fuck')){
+        alert('Warning: this comment has been flagged as offensive');
+        return true;
+    }
+
+    return false;
+}
+function firstLetter(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+}
+
 function submitComment() {
     const inputField = document.getElementById('name');
-    const name = inputField.value;
+    let name = inputField.value;
     const textArea = document.getElementById('msg');
-    const msg = textArea.value;
+    const msg = textArea.value;   
+    if(doesNotPassAllValidations(name, msg)) return null;
+    if(name.charAt(0) === name.charAt(0).toLowerCase()) {
+        name = firstLetter(name);
+        }
     const comment = document.createElement('section');
     const h3 = document.createElement('h3');
     const p = document.createElement('p');
@@ -15,4 +43,6 @@ function submitComment() {
     commentSection.appendChild(comment);
     inputField.value = null;
     textArea.value = null;
+ 
 }
+
